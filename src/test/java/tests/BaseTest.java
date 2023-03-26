d43c5d4dabba9c2b5ec5e8c33da9d8d94512cd4b
atanas011 <atanas011@yahoo.com>
2023-03-26 14:13:21 +0200

    Parameterized Jobs

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -8,10 +9,24 @@ import org.testng.annotations.BeforeTest;
 public class BaseTest {
 
     protected WebDriver driver;
+    String browser = System.getProperty("browser");
 
     @BeforeTest
     public void setUp() {
-        driver = new EdgeDriver();
+        if (browser != null && !browser.equals("")) {
+            if (browser.equalsIgnoreCase("chrome")) {
+                driver = new ChromeDriver();
+            } else if (browser.equalsIgnoreCase("edge")) {
+                driver = new EdgeDriver();
+            } else {
+                System.out.println("Invalid option - defaulting to Edge");
+                browser = "Edge";
+                driver = new EdgeDriver();
+            }
+        } else {
+            browser = "Edge";
+            driver = new EdgeDriver();
+        }
     }
 
     @AfterTest
